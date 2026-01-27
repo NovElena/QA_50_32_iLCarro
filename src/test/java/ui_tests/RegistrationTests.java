90ce7374b6a2bc72ca2fd90d95f1ef8ebc477da8
NovElena <novokhodskayaelena@gmail.com>
2026-01-27 19:39:13 +0200

    login tests

---
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -0,0 +1,50 @@
+package ui_tests;
+
+import dto.User;
+import manager.ApplicationManager;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.PopUpPage;
+import pages.RegistrationPage;
+
+import java.util.Random;
+import static utils.UserFactory.*;
+
+
+public class RegistrationTests extends ApplicationManager {
+    RegistrationPage registrationPage;
+
+    @BeforeMethod
+    public void goToRegistrationPage() {
+        new HomePage(getDriver()).clickBtnSignUp();
+        registrationPage = new RegistrationPage(getDriver());
+    }
+
+    @Test
+    public void registrationPositiveTest() {
+        int i = new Random().nextInt(1000);
+        User user = User.builder()
+                .firstName("UUU")
+                .lastName("PPP")
+                .email("lmkjiu"+i+"@defrt.bhy")
+                .password("Pqwerty453!")
+                .build();
+        registrationPage.typeRegistrationForm(user);
+        registrationPage.clickCheckBoxWithActions();
+        registrationPage.clickBtnYalla();
+        Assert.assertTrue(new PopUpPage(getDriver())
+                .isTextInPopUpMessagePresent("You are logged in success"));
+    }
+
+    @Test
+    public void registrationPositiveTest_WithFaker() {
+        User user = positiveUser();
+        registrationPage.typeRegistrationForm(user);
+        registrationPage.clickCheckBoxWithActions();
+        registrationPage.clickBtnYalla();
+        Assert.assertTrue(new PopUpPage(getDriver())
+                .isTextInPopUpMessagePresent("You are logged in success"));
+    }
+}
